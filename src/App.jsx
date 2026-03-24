424cdf906991f4374a12f9d6e73996c8a2e27ed3
siyasiyana274 <siyasiyana274@gmail.com>
2026-03-24 14:32:15 +0530

    error clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import './App.css'
 
 function App() {
@@ -7,12 +6,8 @@ function App() {
 
   return (
     <>
-      
+  
 
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-          
                   
                  
                
