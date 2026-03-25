65ff0d375e656191c61a14f89cfcdb3371239cf9
mohanapiriyan-05 <mohanapiriyan05oms@gmail.com>
2026-03-25 14:37:54 +0530

    alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import './App.css'
-import PaymentDetails from './Component/PaymentDetails/PaymentDetails'
-import StudentsAttendance from './Component/StudentsAttendance/StudentsAttendance'
+import Listmanagement from './Component/Studentsmanagementlist/Listmanagement'
 
 
 function App() {
   return (
     <>
-      
-<PaymentDetails/>
-<StudentsAttendance/>
-
-{/* <StudentsAttendance/>
-                   */}
-                   
-                 
+  
+           <Listmanagement/>         
                
     </>
   )
